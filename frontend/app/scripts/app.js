a8887b764fc9dfdaf78af1ada96cdb9efc099bbc
Erik Donohoo <erikdonohoo@gmail.com>
2015-02-04 18:33:23 -0700

    add files for getting to foursquare

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('foursquare', [
+	'ngSanitize',
+	'angular-oauth2'
+])
+.config(['$oauth2Provider', function ($oauth2) {
+
+	$oauth2.configure({
+		clientId: 'DU0NBQTNEANSZ0PZVQT5VKEPGJQK0DJYZHHUA1UUV1WJZGK0',
+		redirectUri: true,
+		autoAuth: false,
+		oauth2Url: 'https://foursquare.com/oauth2/authorize'
+	});
+}])
+
+.run(['$rootScope', '$http', '$oauth2', function ($scope, $http, $oauth2) {
+
+	// Expose app version info
+	$http.get('version.json').success(function (v) {
+		$scope.version = v.version;
+		$scope.appName = v.name;
+	});
+
+	$scope.$oauth2 = $oauth2;
+
+	$scope.getStuff = function () {
+		$http.get('https://api.foursquare.com/v2/users/self/checkins?v=20140805&oauth_token=' + $oauth2.getToken().access_token)
+		.success(function (data) {
+			console.log(data);
+		});
+	};
+}]);
